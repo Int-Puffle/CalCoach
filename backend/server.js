6af40b7c77809650fc090e6fdb2ca58c5bd0cc37
Puffle <183334177+Int-Puffle@users.noreply.github.com>
2026-07-05 17:28:42 -0400

    Connect backend to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+client.connect()
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('CalCoach API is running');
